489e4be96c73cfa1f4fb0b21b43e1fb198c318cc
github username <github email address>
2020-05-14 11:01:36 -0400

    homepage and about page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < Sinatra::Base
 
+    set :views, "app/views"
+
+    get "/" do
+        erb :homepage
+    end
+
+    get "/about" do
+        erb :about
+    end
+
 end
