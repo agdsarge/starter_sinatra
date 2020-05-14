22cea0afd9cd61a50d60b54ef7939bd3a546a186
github username <github email address>
2020-05-14 11:40:21 -0400

    create and show artist

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -1,3 +1,25 @@
 class ArtistsController < Sinatra::Base
 
+    set :views, "app/views/artists"
+
+    get "/artists/new" do
+        erb :new_artist
+    end
+
+    post "/artists" do
+        artist = Artist.create(name: params[:name], hometown: params[:hometown], instrument: params[:instrument])
+        redirect "/artists/#{artist.id}"
+    end
+
+    get "/artists/:id/edit" do
+        @artist = Artist.find(params[:id])
+        erb :edit_artist
+    end
+
+    get "/artists/:id" do
+        @artist = Artist.find(params[:id])
+        erb :show_artist
+    end
+
+
 end
